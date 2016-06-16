7f3e55dc60aa21939396ca773915ff5cdd80c19b
Stefan Walkner <stefan@arkulpa.at>
2016-06-16 13:01:49 +0200

    Initial Commit

---
+++ StoryboardWithCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryboardWithCocoapods
+//
+//  Created by Stefan Walkner on 16.06.16.
+//  Copyright © 2016 Allianz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoryboardWithCocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryboardWithCocoapods
+//
+//  Created by Stefan Walkner on 16.06.16.
+//  Copyright © 2016 Allianz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
